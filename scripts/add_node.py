bd4a5573c7e00ad483ac96e371cf089a15497c33
Brian Carpio <bcarpio@thetek.net>
2012-08-03 18:51:35 -0700

    added multi node inheritance, still need to check for inheritance before allowing a node to be removed, I will do this before I commit back to master

--- scripts/add_node.py
+++ scripts/add_node.py
@@ -21,28 +21,38 @@ def connect_mongodb():
 
 def main():
 
+	col = connect_mongodb()
 	cmd_parser = argparse.ArgumentParser(description='Add Nodes To Mongodb ENC')
 	cmd_parser.add_argument('-n', '--node', dest='puppet_node', help='Puppet Node Hostname', required=True)
-	cmd_parser.add_argument('-c', '--class', dest='puppet_classes', help='Can specify multiple classes each with -c', action='append', required=True)
+	cmd_parser.add_argument('-c', '--class', dest='puppet_classes', help='Can specify multiple classes each with -c', action='append')
 	cmd_parser.add_argument('-p', '--param', dest='puppet_param', help='Can specify multiple paramaters each with -p', action='append')
 	cmd_parser.add_argument('-i', '--inherit', dest='puppet_inherit', help='Define a node to inherit classes from', action='store', default='default')
 	cmd_parser.add_argument('-e', '--environment', dest='environment', help='Optional, defaults to "production"', default='production')
 	args = cmd_parser.parse_args()
 
-	c = {}
-	for pclass in args.puppet_classes:
-		c[pclass] = ''
+	if args.puppet_node == args.puppet_inherit:
+		print "ERROR: Node name and inherit name can not be the same"
+		sys.exit(1)
 
-	d = { 'node' : args.puppet_node, 'enc' : { 'classes': c , 'environment' : args.environment }}
+	if args.puppet_classes:
+
+		c = {}
+		for pclass in args.puppet_classes:
+			c[pclass] = ''
+		d = { 'node' : args.puppet_node, 'enc' : { 'classes': c , 'environment' : args.environment }}
+	else:
+		d = { 'node' : args.puppet_node, 'enc' : { 'environment' : args.environment}}
 
 	if args.puppet_param:
 		args.puppet_param = dict([arg.split('=') for arg in args.puppet_param])
 		d['enc']['parameters'] = args.puppet_param
 
 	if args.puppet_inherit:
+		ck = col.find_one({ "node" : args.puppet_inherit})
+		if not ck:
+			print "ERROR: Inherit node does not exist, please add "+args.puppet_inherit+" and then retry"
+			sys.exit(1)
 		d['inherit'] = args.puppet_inherit
-
-	col = connect_mongodb()
 
 	check = col.find({ 'node' : args.puppet_node },{'node': 1})
 	for document in check:
