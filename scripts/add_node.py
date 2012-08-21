72c5583a65f72f008e54dc1d42300a10bf344576
Brian Carpio <bcarpio@thetek.net>
2012-08-21 13:23:02 -0700

    updated add node so only default was set to inherit when adding a node, apparently setting inherit to default was also screwing up the append feature if you didn't always define inherit

--- scripts/add_node.py
+++ scripts/add_node.py
@@ -37,7 +37,7 @@ def main():
     cmd_parser.add_argument('-n', '--node', dest='puppet_node', help='Puppet Node Hostname', required=True)
     cmd_parser.add_argument('-c', '--class', dest='puppet_classes', help='Can specify multiple classes each with -c', action='append')
     cmd_parser.add_argument('-p', '--param', dest='puppet_param', help='Can specify multiple parameters each with -p', action='append')
-    cmd_parser.add_argument('-i', '--inherit', dest='puppet_inherit', help='Define a node to inherit classes from', action='store', default='default')
+    cmd_parser.add_argument('-i', '--inherit', dest='puppet_inherit', help='Define a node to inherit classes from', action='store')
     cmd_parser.add_argument('-e', '--environment', dest='environment', help='Optional, defaults to "production"', default='production')
     args = cmd_parser.parse_args()
 
@@ -61,6 +61,9 @@ def main():
             sys.exit(1)
 
     if args.puppet_action == 'new':
+        if not args.puppet_inherit:
+          print "ERROR: You Need To Define PUPPET_INHERIT"
+          sys.exit(1)
         check = col.find({ 'node' : args.puppet_node }, {'node': 1})
         for document in check:
             node = document['node']
